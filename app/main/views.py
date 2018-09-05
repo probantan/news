e777a3c99663699e9e4a78d36a490ac884701152
Protus bantan <protus.bantan@gmail.com.com>
2018-09-05 10:38:35 +0300

    updated my README.md

--- app/main/views.py
+++ app/main/views.py
@@ -6,7 +6,9 @@ from ..models import Sources
 #views
 @main.route('/')
 def index():
-	
+	'''
+	view root page function that returns the index the page and its data
+	'''
 	sources = get_sources('business')
 	sports_sources = get_sources('sports')
 	technology_sources = get_sources('technology')
